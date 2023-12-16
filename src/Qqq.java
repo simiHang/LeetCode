6893c95571c63c5336756adc8eb3112877be7cd6
semiHang <qyhbll096@163.com>
2023-12-16 17:28:40 +0800

    Initial commit

---
+++ src/Qqq.java
@@ -0,0 +1,11 @@
+import course.arrayandstring.FindInsertPlace;
+
+public class Qqq {
+
+    public static void main(String[] args) {
+        FindInsertPlace findInsertPlace = new FindInsertPlace();
+
+        int[] nums = new int[]{2, 1, -1};
+        System.out.println(findInsertPlace.searchInsert(nums, 1));
+    }
+}
---
+++ src/course/arrayandstring/FindInsertPlace.java
@@ -0,0 +1,8 @@
+package course.arrayandstring;
+
+public class FindInsertPlace {
+
+    public int searchInsert(int[] nums, int target) {
+        return 1110;
+    }
+}
